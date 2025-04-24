2f9702539c139390defd7d6fdb95e6f58d9410b0
SW-64 <ettjddns6@gmail.com>
2025-04-24 14:24:56 +0900

    FEAT : 외부 명소 사진 API 조회 구현

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,9 @@ import { spotRouter } from './spot.router.js';
 import { userRouter } from './user.router.js';
 import { authRouter } from './auth.router.js';
 import { requireAccessToken } from '../middlewares/require-access-token.middlewares.js';
-import { cityRouter } from './city.router.js';
 
 const apiRouter = express.Router();
 
-apiRouter.use('/api/cities', requireAccessToken, cityRouter);
 apiRouter.use('/api/spots', requireAccessToken, spotRouter);
 apiRouter.use('/api/users', requireAccessToken, userRouter);
 apiRouter.use('/api/auth', authRouter);

45710dca17b7d1da8c5ca4a7015e1658cb39b0ea
SW-64 <ettjddns6@gmail.com>
2025-05-05 21:00:31 +0900

    UPDATE : test Routing 반환값 수정

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(apiRouter);
 app.use(globalErrorHandler);
 app.get('/', (req, res) => {
-  return res.json('hello world test');
+  return res.json('hello world test!!!');
 });
 
 app.listen(port, async () => {

6d64d1729d4842eb5b0ce86a92178b664b1c9418
SW-64 <ettjddns6@gmail.com>
2025-07-02 21:35:06 +0900

    REFACTORING : 로그인 service 코드 파트 개선

---
+++ prisma/seed.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await bcrypt.hash('1234', 10);
+
+  for (let i = 21; i <= 50; i++) {
+    for (let j = 1; j <= 3; j++) {
+      await prisma.interaction.create({
+        data: {
+          userId: i,
+          spotId: j, // 1~3번 spot에 북마크
+          type: 'BOOKMARK',
+        },
+      });
+    }
+  }
+}
+
+main()
+  .then(() => {
+    console.log('✅ 유저 더미 데이터 생성 완료');
+    prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error(e);
+    prisma.$disconnect();
+    process.exit(1);
+  });
